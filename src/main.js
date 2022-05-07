ba7de7a8e4a3024900576c17aa5fc19987976cc9
goddessIU <2581606394@qq.com>
2022-05-07 22:20:53 +0800

    fix<src/main, src/index.html>: 为popup页面布局以及设置合适的vue挂载容器

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import { createApp, DefineComponent } from "vue";
 import Popup from "./Popup.vue";
 
-const MOUNT_EL_ID = "attonex_clipper";
+const MOUNT_EL_ID = "readHelperPopup";
 
 let mountEl = document.getElementById(MOUNT_EL_ID);
+
 if (mountEl) {
     mountEl.innerHTML = "";
+} else if (mountEl === null) {
+    mountEl = document.createElement("div");
+    mountEl.setAttribute("id", MOUNT_EL_ID);
 }
-mountEl = document.createElement("div");
-mountEl.setAttribute("id", MOUNT_EL_ID);
+
 document.body.appendChild(mountEl);
-const vm = createApp(Popup).mount(mountEl);
+
+createApp(Popup).mount(mountEl);
